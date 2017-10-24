5ddeacbdc608eca83b88bca39311ba84b64ebb00
Anil Wadghule <anildigital@gmail.com>
2017-10-24 17:38:31 +0530

    Change supervisor strategy

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -14,6 +14,6 @@ defmodule KV.Supervisor do
       KV.BucketSupervisor
     ]
 
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.init(children, strategy: :one_for_all)
   end
 end
